6ed9ff51cacb1d03b31887a919ab388408c5eac5
ReutUzan <143487886+ReutUzan@users.noreply.github.com>
2023-09-19 14:13:22 +0300

    Add files via upload

---
+++ cachemap.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <map>
+#include <vector>
+#include "pointers.h"
+
+using namespace std;
+
+template <class K, class V>
+class CacheMemory
+{
+public:
+    CacheMemory()
+    {
+        std::map<K, V> cacheMemory;
+    };
+
+    void add(const K& key, const V& value)
+    {
+        if (nullptr != cacheMemory[key])
+        {
+            throw DuplicateKeyException;
+        }
+        CacheMemory[key] = value;
+    }
+
+    void erase(const K& key)
+    {
+        if (nullptr != CacheMemory.find(key))
+        {
+            CacheMemory.erase(key);
+        }
+
+        else
+        {
+            throw ObjectNotExistException;
+        }
+    }
+
+    std::shared_ptr<V> get(const K& key)
+    {
+        return CacheMemory.find(key);
+    }
+
+    std::vector<V> getCacheValues()
+    {
+        for (auto it = CacheMemory.begin(); CacheMemory.end(); it+)
+        {
+            std::vector<V> newVec;
+            newVec.push_back(it);
+            return newVec;
+        };
+    }
+};
